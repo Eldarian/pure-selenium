6b0a636b70012b321d750448e786b7e2609126ba
Dmitry Plyshevsky <dplyshevsky@Dmitrys-MacBook-Pro.local>
2021-09-22 18:39:32 +0300

    refresh and wait tests

---
+++ src/main/java/com/eldarian/pureselenium/AlertsPage.java
@@ -0,0 +1,29 @@
+package com.eldarian.pureselenium;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AlertsPage {
+    WebDriver driver;
+
+    @FindBy(id="timerAlertButton")
+    private WebElement timeAlertButton;
+
+    public AlertsPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public Alert clickTimedAlert() {
+        timeAlertButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        return wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+
+}
