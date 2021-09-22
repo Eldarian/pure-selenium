b82ce3fef4b0b6f74c67b7fe99e1b46f6215d9ce
Dmitry Plyshevsky <dplyshevsky@Dmitrys-MacBook-Pro.local>
2021-09-22 11:48:28 +0300

    simple logging and test assertion

--- src/test/java/com/eldarian/pureselenium/BasicTest.java
+++ src/test/java/com/eldarian/pureselenium/BasicTest.java
@@ -1,15 +1,26 @@
 package com.eldarian.pureselenium;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.Logger;
 
 public class BasicTest {
 
+    private static final Logger LOGGER = Logger.getLogger(BasicTest.class);
     WebDriver driver;
+
+    @BeforeClass
+    public void configureLogger() {
+        //PropertyConfigurator.configure("src/main/resources/log4j.properties");
+    }
 
     @BeforeMethod
     public void initDriver() {
@@ -22,7 +33,7 @@ public class BasicTest {
         DemoQAHomePage homePage = new DemoQAHomePage(driver);
         driver.manage().window().maximize();
         homePage.clickElementsCard();
-        driver.getTitle();
+        Assert.assertEquals(driver.findElement(By.className("main-header")).getText(), "Elements");
     }
 
     @AfterMethod
